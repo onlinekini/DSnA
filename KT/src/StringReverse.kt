70c460fa3f9c74d3088aeb7c4236a5c45a6f97e0
onlinekini@gmail.com <onlinekini@gmail.com>
2019-03-01 09:25:37 +0530

    fixed my first KT

--- KT/src/StringReverse.kt
+++ KT/src/StringReverse.kt
@@ -1,13 +1,16 @@
 fun main (args : Array<String>) {
 
+    print(reverseString("Vivekanand !!"))
+
 }
 
 
-fun reverse (inputStr : String, pos : Int ) : String {
+fun reverseString (inputStr : String, pos: Int = inputStr.length,  output : String = "" ) : String {
+    //println(output)
 
     if (pos == 0) {
         return output
     } else {
-        output = reverse(inputStr, pos-1) + inputStr[pos -1]
+        return reverseString(inputStr, pos-1,  output + inputStr[pos -1])
     }
 }
